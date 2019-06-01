405f565167ee7343b5e01f08b5fa122b00854389
Robson Alves Nogueira <rbsalvesmail@gmail.com>
2019-06-01 13:00:56 -0300

    snake case on functions

--- website_wordcloud.py
+++ website_wordcloud.py
@@ -9,35 +9,35 @@ import matplotlib.pyplot as plt
 
 def main():
     url = '' if len(sys.argv) == 1 else sys.argv[1]
-    html = getUrlContents(url)
-    wordcloud = generateWordcloud(html)
-    saveWordcloudToImage(wordcloud, 'wordcloud.png')
+    html = get_url_contents(url)
+    wordcloud = generate_word_cloud(html)
+    save_word_cloud_to_image(wordcloud, 'wordcloud.png')
 
-def generateWordcloud(html):
+def generate_word_cloud(html):
     soup = BeautifulSoup(html, "html.parser")
-    text = cleanHtmlText(soup.get_text())
+    text = clean_html_text(soup.get_text())
     return WordCloud(background_color='white').generate(text)
 
-def saveWordcloudToImage(wordcloud, filename):
+def save_word_cloud_to_image(wordcloud, filename):
     folder = os.path.dirname(os.path.abspath(__file__))
     wordcloud.to_file(folder+'/'+filename)
     return folder+filename
 
-def cleanHtmlText(html):
+def clean_html_text(html):
     html = re.sub("\s+", ' ', html)
     html = re.sub("<.*?>", '', html)
     html = re.sub(r'\\n', ' ', html)
     return html
 
-def getUrlContents(url):
+def get_url_contents(url):
     if (len(url) == 0):
         return 'Url not provided'
-    if (not isValidUrl(url)):
+    if (not is_valid_url(url)):
         return 'Invalid url'
     req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"}) 
     return str(urllib.request.urlopen(req).read().decode('utf-8'))
 
-def isValidUrl(url):
+def is_valid_url(url):
     regex = re.compile(r'^(http|https)://', re.IGNORECASE)
     if re.match(regex, url):
         return True
